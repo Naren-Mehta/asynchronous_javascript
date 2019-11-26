448aa4a216ae560964c0bfa298fec8e6d70d25b0
Narendra Singh Mehta <NarendraM@quinnox.com>
2019-11-26 22:21:32 +0530

    Working on example

--- 4.AsyncAndAwait/6.sequentialVsParallelExecution.js
+++ 4.AsyncAndAwait/6.sequentialVsParallelExecution.js
@@ -32,14 +32,44 @@ async function oneByOne() {
 async function inParallel() {
     const promise1 = printNumber1();
     const promise2 = printNumber2();
-
-
     const number1 = await promise1;
     const number2 = await promise2;
 
     console.log(number1, number2);
-
-    
 }
 
-inParallel();
+// inParallel();
+
+//====================================
+//inParallel - with destructor
+
+
+async function inParallel1() {
+    const promise1 = printNumber1();
+    const promise2 = printNumber2();
+    const [number1, number2]= [await promise1,await promise2]
+
+    console.log(number1, number2);
+}
+
+// inParallel1();
+
+//====================================
+
+async function inParallel2() {
+    const promise1 = printNumber1();
+    const promise2 = printNumber2();
+    console.log(await promise1,await promise2);
+}
+
+inParallel2();
+
+
+
+
+
+
+
+
+
+
